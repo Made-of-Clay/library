a109fc6fd0cce1fb96e40d82b909b633ffe31cf2
Adam Leis <adam.m.leis@gmail.com>
2025-02-17 11:58:50 -0500

    re-add details

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func indexHandler(w http.ResponseWriter, _r *http.Request) {
 
 	tmpl := template.Must(template.ParseFiles("templates/shell.html", "templates/index.html"))
 
-	fmt.Println("-----", tmpl.Templates())
 	err := tmpl.ExecuteTemplate(w, "shell.html", nil)
 	if err != nil {
 		log.Print(err.Error())
@@ -25,7 +24,7 @@ func detailHandler(w http.ResponseWriter, _r *http.Request) {
 
 	tmpl := template.Must(template.ParseFiles("templates/shell.html", "templates/detail.html"))
 
-	err := tmpl.ExecuteTemplate(w, "base", nil)
+	err := tmpl.ExecuteTemplate(w, "shell.html", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -33,8 +32,7 @@ func detailHandler(w http.ResponseWriter, _r *http.Request) {
 }
 
 func main() {
-	// TODO resume this after I better understand template parsing
-	// http.HandleFunc("/detail", detailHandler)
+	http.HandleFunc("/detail", detailHandler)
 	http.HandleFunc("/", indexHandler)
 
 	// Write a 404 handler for all unmatched routes
@@ -50,19 +48,11 @@ func main() {
 	// })
 
 	// TODO: Implement logging middleware to log all incoming requests, their methods, and their URLs
-	// log.SetFlags(log.Lshortfile | log.LstdFlags) // Log with file and line number
+	log.SetFlags(log.Lshortfile | log.LstdFlags) // Log with file and line number
 
-	// TODO: Implement rate limiting middleware to limit the number of requests a client can make to the server within a certain time frame
 	// TODO: Implement authentication middleware to ensure only authorized clients can access the server's endpoints and resources
 
 	// TODO: Implement caching middleware to store and serve static files from a cache for faster response times
-	// TODO: Implement load balancing middleware to distribute incoming requests across multiple server instances to improve performance and scalability
-
-	// Start the server
-	// TODO: Implement error handling for starting the server, logging errors, etc.
-	// log.Fatal(http.ListenAndServe(":8008", nil))
-	// log.Fatal(http.ListenAndServeTLS(":8008", "./cert.pem", "./key.pem", nil)) // for HTTPS with TLS certificates
-	// log.Fatal(http.ListenAndServe(":8008", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Starting server on localhost:8008 ...")
 	log.Fatal(http.ListenAndServe(":8008", nil))
