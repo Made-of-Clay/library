9fe6f3b63adc5c312cd48d31de7f8715f420f032
Adam Leis <adam.m.leis@gmail.com>
2025-02-17 11:46:38 -0500

    finally got templates & static assets working

--- main.go
+++ main.go
@@ -4,71 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"text/template"
 )
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s", r.URL.Path[1:])
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	fmt.Println("---- title" + title)
-	p, err := loadPage(title)
-	if err != nil {
-		fmt.Println("OH CRAP OH CRAP OH CRAP >>> ", err)
-		fmt.Fprintf(w, "ERROR: %s", err.Error())
-		return
-	}
-
-	t, _ := template.ParseFiles("view.html")
-	t.Execute(w, p)
-}
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-
-	t, _ := template.ParseFiles("edit.html")
-	t.Execute(w, p)
-
-	// fmt.Fprintf(w, `<h1>Editing %s</h1>
-	// <form action="/save/" method="POST"
-	// 	<textarea name=body>%s</textarea>
-	// 	<button>Save</button>
-	// </form?`,
-	// 	p.Title, p.Body)
-}
 
 func indexHandler(w http.ResponseWriter, _r *http.Request) {
 	fmt.Println("Serving route /")
 
 	tmpl := template.Must(template.ParseFiles("templates/shell.html", "templates/index.html"))
 
-	foo := &Page{Title: "bar "}
-	err := tmpl.ExecuteTemplate(w, "shell.html", foo)
+	fmt.Println("-----", tmpl.Templates())
+	err := tmpl.ExecuteTemplate(w, "shell.html", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -80,8 +25,7 @@ func detailHandler(w http.ResponseWriter, _r *http.Request) {
 
 	tmpl := template.Must(template.ParseFiles("templates/shell.html", "templates/detail.html"))
 
-	foo := &Page{Title: "bar "}
-	err := tmpl.ExecuteTemplate(w, "shell.html", foo)
+	err := tmpl.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -91,17 +35,34 @@ func detailHandler(w http.ResponseWriter, _r *http.Request) {
 func main() {
 	// TODO resume this after I better understand template parsing
 	// http.HandleFunc("/detail", detailHandler)
-	// http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", indexHandler)
 
-	// Tutorial
-	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	// p1.save()
-	// p2, _ := loadPage("TestPage")
-	// fmt.Println(string(p2.Body))
-	// http.HandleFunc("/", handler)
-	http.HandleFunc("/view/", viewHandler)
-	// http.HandleFunc("/save/", saveHandler)
-	http.HandleFunc("/edit/", editHandler)
+	// Write a 404 handler for all unmatched routes
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/favicon.ico")
+	})
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	// Return 404 for any non-static and non root or detail routes
+	// http.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//     http.Error(w, "Not Found", http.StatusNotFound)
+	// })
+
+	// TODO: Implement logging middleware to log all incoming requests, their methods, and their URLs
+	// log.SetFlags(log.Lshortfile | log.LstdFlags) // Log with file and line number
+
+	// TODO: Implement rate limiting middleware to limit the number of requests a client can make to the server within a certain time frame
+	// TODO: Implement authentication middleware to ensure only authorized clients can access the server's endpoints and resources
+
+	// TODO: Implement caching middleware to store and serve static files from a cache for faster response times
+	// TODO: Implement load balancing middleware to distribute incoming requests across multiple server instances to improve performance and scalability
+
+	// Start the server
+	// TODO: Implement error handling for starting the server, logging errors, etc.
+	// log.Fatal(http.ListenAndServe(":8008", nil))
+	// log.Fatal(http.ListenAndServeTLS(":8008", "./cert.pem", "./key.pem", nil)) // for HTTPS with TLS certificates
+	// log.Fatal(http.ListenAndServe(":8008", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Starting server on localhost:8008 ...")
 	log.Fatal(http.ListenAndServe(":8008", nil))
